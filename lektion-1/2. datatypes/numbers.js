8c4f2b4179fce4de1630a08714d429d795f71557
Jesper Ekerling <jesper@forme.se>
2023-10-24 11:29:23 +0200

    Test

---
+++ lektion-1/2. datatypes/numbers.js
@@ -0,0 +1,29 @@
+/*
+    NUMBERS
+
+    +   =   adding
+    -   =   subtracting
+    *   =   multiply
+    /   =   divide
+    **  =   double multiply 
+    Math.   =   A lot of math functions
+    %       =   division remainder
+
+    ++      =   add 1
+    --      =   subtract 1
+    +-      =   Add to the current value
+    -=      =   Remove from the current value
+*/
+
+
+let num1 = 10;
+let num2 = 2.5
+
+num1 = num1 + 1;
+
+num1 = num1 += 2;
+console.log(num1);
+
+let string1 = "20";
+
+console.log(num1 + +string1); // Converts string1 to a string wiht a + sign
