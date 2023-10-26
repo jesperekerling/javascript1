37f8fbea5d94b5b8f8ee4ebd7be731bb498f054d
Jesper Ekerling <jesper@forme.se>
2023-10-26 13:54:20 +0200

    Update

--- lektion-2/4-if-statements/script.js
+++ lektion-2/4-if-statements/script.js
@@ -14,3 +14,18 @@ let string = '13'
 
 console.log(13 == '13') //loose equality
 console.log(13 === '13') //strict equality (same type and value)
+
+console.log('heej')
+console.log(11 !== '11')
+
+
+const user = {
+    name: 'jesper',
+    active: true
+}
+
+if (user.active) {
+    console.log(`${user.name} is active`)
+}
+
+
