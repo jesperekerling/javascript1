02245aeb1f807d5eff21a74b910420b15b2d9c38
Jesper Ekerling <jesper@forme.se>
2023-10-26 13:37:45 +0200

    Updates

--- lektion-2/3-examples/script.js
+++ lektion-2/3-examples/script.js
@@ -1,28 +1,56 @@
 const leftNames = ['Kalle', 'Pelle', 'Jesper', 'Pelle', 'Olle']
 const rightNames = ["Kalle2", "Pelle", "Jesper", "Pelle", "Olle"]
 
-const leftBox = document.getElementById('box-1');
+const leftBox = document.getElementById('box');
 const rightBox = document.getElementById('box-2');
 
 function renderNames () {
     leftBox.innerHTML = ''
     rightBox.innerHTML = ''
 
-    leftNames.forEach(name => {
+    leftNames.forEach(function(name, i) {
         const p = document.createElement('p')
         p.classList.add('item')
         p.innerText = name
 
-        p.addEventListener('click', () => {
-            consolg.log(p)
+        p.addEventListener('click', function() {
+            const name = leftNames.splice(i, 1)
+            console.log(name)
+            rightNames.push(name)
+            renderNames()
         })
         leftBox.appendChild(p)
     });
-    rightNames.forEach(name => {
+
+
+    rightNames.forEach(function(name, i) {
         const p = document.createElement('p')
         p.classList.add('item')
         p.innerText = name
 
+        p.addEventListener('click', function() {
+            const name = rightNames.splice(i, 1)
+            console.log(name)
+            leftNames.push(name)
+            renderNames()
+        })
         rightBox.appendChild(p)
     });
 }
+renderNames()
+
+const input = document.getElementById('input')
+const btnLeft = document.getElementById('btn-l')
+const btnRight = document.getElementById('btn-r')
+
+btnLeft.addEventListener('click', function() {
+    leftNames.unshift(input.value)
+    input.value = ''
+    renderNames()
+})
+
+btnRight.addEventListener('click', function() {
+    rightNames.unshift(input.value)
+    input.value = ''
+    renderNames()
+})
