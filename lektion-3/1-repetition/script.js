874d96a9937a38cbe6f0c804be1642f802c5dd87
Jesper Ekerling <jesper@forme.se>
2023-10-31 09:15:48 +0100

    Loops repetition

--- lektion-3/1-repetition/script.js
+++ lektion-3/1-repetition/script.js
@@ -1,4 +1,27 @@
 const names = ["Kalle", "Pelle", "Olle", "Nisse"];
+users = [
+    {name: "Kalle", age: 20},
+    {name: "Pelle", age: 30},
+    {name: "Olle", age: 40},
+]
+
+
+let todos = [
+    {id: 1, text: "Köp mjölk", isCompleted: true},
+    {id: 2, text: "Köp ägg", isCompleted: false},
+    {id: 3, text: "Köp kaffe", isCompleted: true},
+    {id: 4, text: "Köp kaffe", isCompleted: false},
+]
+
+// A filter returns a new array, can manipulate the array
+todos = todos.filter(function(todo) {
+    return todo.id !== 3;
+})
+// Om man ska använda const istället så använder man splice-funktionen
+
+
+
+console.log(todos)
 
 for(let i = 0; i < names.length; i++) {
     console.log(names[i]);
@@ -10,3 +33,12 @@ names.forEach(function(name, index) {
     console.log(index + ' ' + name);
     console.log(index);
 });
+
+const newUsers = users.map(function(user, index) {
+    //user.name = user.name.toUpperCase();
+
+    //return user;
+
+    return user.age;
+})
+console.log(newUsers)
