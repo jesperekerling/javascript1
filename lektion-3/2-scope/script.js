9a263c246352ec128361cd8e9e66aa755b2cb388
Jesper Ekerling <jesper@forme.se>
2023-10-31 11:15:38 +0100

    Update

--- lektion-3/2-scope/script.js
+++ lektion-3/2-scope/script.js
@@ -20,3 +20,66 @@ console.log('Outside code blockss ' + age);
     let fName = 'Hans';
     console.log(fName)
 }
+
+
+// Functions
+// To be able to reuse code
+
+/*
+Do not repeat yourself
+*/
+
+// Try to use funtions for specific taks, do not mix many tasks in one function
+
+
+
+// Hoisting - function will be run at the top of the file
+hello()
+
+// Declaration of function
+function hello() {
+    console.log('Hello maaan')
+}
+// Run the function
+hello()
+
+hello()
+
+const greet = function () {
+    console.log('hello hey from greet')
+}
+greet()
+
+
+// Function of a function is to return a value
+
+const test = function() {
+    console.log('test');
+    return `Testing testingg` // When writing return the function will stop
+}
+const test1 = test()
+
+let firstNamy = 'Jesper'
+
+// parameter
+const greet2 = function(name) {
+    console.log('Hello ' + name + ' from greet2')
+    return `hello ${name} from greetyyy`
+}
+const abc = greet2('Fredrik')
+greet2(firstNamy)
+greet2('Janne')
+
+console.log(abc)
+
+
+
+// Function with two parameters
+const getFullName = function(firstName, lastName) {
+    const fullName = `${firstName} ${lastName}`
+    console.log(fullName)
+    return fullName
+}
+const user1 = getFullName('Jesper', 'Ekerling')
+const user2 = getFullName('Fredrik', 'Andersson')
+const user3 = getFullName('Fredrik', 'Hansson')
