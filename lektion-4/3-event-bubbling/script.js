bdbfafd84e855e1ef3dfac2feff0644042663028
Jesper Ekerling <jesper@forme.se>
2023-11-02 13:39:58 +0100

    Update

--- lektion-4/3-event-bubbling/script.js
+++ lektion-4/3-event-bubbling/script.js
@@ -8,7 +8,7 @@ const btn = document.querySelector('#btn');
 // e.target = the element that we clicked on
 // e.currentTarget = the element that the event listener is attached to
 
-/* btn.addEventListener('click', function (e) {
+btn.addEventListener('click', function (e) {
     console.log('btn target: ', e.target);
     console.log('btn currentTarget: ', e.currentTarget);
 });
@@ -21,7 +21,7 @@ div1.addEventListener('click', function (e) {
 div2.addEventListener('click', function (e) {
     console.log('div2 target: ', e.target);
     console.log('div2 currentTarget: ', e.currentTarget);
-}); */
+}, true); 
 
 document.body.addEventListener('click', function (e) {
     console.log('body target: ', e.target);
@@ -31,3 +31,37 @@ document.body.addEventListener('click', function (e) {
         console.log('btn was clicked');
     }
 });
+
+/* 
+const names = ['Mahmud', 'Kalle', 'Erik', 'Sven'];
+const output = document.querySelector('#output');
+
+names.forEach(function (name) {
+document.querySelector('#output').innerHTML  += '<ul>';
+document.querySelector('#output').innerHTML  += '<li>' + name + '</li>';
+document.querySelector('#output').innerHTML  += '</ul>';
+}   );
+
+output.addEventListener('click', function (e) {
+    console.log(e.target);
+    e.target.remove()
+}); */
+
+
+// Capture phase is body --> div2 --> div1 --> btn
+// Bubbling phase is btn --> div1 --> div2 --> body
+
+
+const user = document.querySelector('.user');
+const userBtn = document.querySelector('.user-btn');
+
+user.addEventListener('click', function (e) {
+    user.classList.toggle('active');
+}  );  
+userBtn.addEventListener('click', function (e) {
+    user.style.border = '3px solid red';
+}   );
+
+// stop bubbling
+// e.stopPropagation(); 
+// prevent bubbling and default behaviour
