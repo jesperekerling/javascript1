6ac06add1459d33bb1db7372bd90708f4e8f41ac
Jesper Ekerling <jesper@forme.se>
2023-11-02 13:02:54 +0100

    Update

--- lektion-4/2-events/script.js
+++ lektion-4/2-events/script.js
@@ -40,5 +40,34 @@ btn.addEventListener("mouseleave", () => {
 const square = document.querySelector("#square");
 square.addEventListener("mousemove", event => {
     square.style.backgroundColor = `rgb(${event.offsetX}, ${event.offsetY}, 100)`;
-}
-);
+});
+
+/* const username = document.querySelector("#username");
+console.log(username);
+
+username.addEventListener("keyup", () => {
+    console.log(username);
+}   );
+
+
+
+username.addEventListener('focus', e () => { 
+    console.log(e)
+    username.classList.add("focus-red")
+}   );
+
+
+ */
+
+document.querySelector('#link').addEventListener("click", e => {
+    e.preventDefault();
+    console.log("Link clicked, but redirects to wrong page");
+    window.location.href ="https://www.google.com"
+}   );
+
+document.querySelector('#loginForm').addEventListener("click", e => {
+    e.preventDefault();
+    if(username.value === "") {
+        console.log("Username is empty");
+    }   }   );
+    
