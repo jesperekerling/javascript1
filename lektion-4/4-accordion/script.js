64f97998c66dabe706b8f1466b5043bd660a066b
Jesper Ekerling <jesper@forme.se>
2023-11-02 15:29:12 +0100

    Update

--- lektion-4/4-accordion/script.js
+++ lektion-4/4-accordion/script.js
@@ -1,16 +1,25 @@
-const questions = document.querySelectorAll('.question');
+const questions = document.querySelectorAll('.question')
 
-questions.forEach((question) => {
-    question.addEventListener('click', () => {
-        //question.nextElementSibling.
-    //question.nextElementSibling.className = 'open'
-    const parent = question.parentElement;
-    console.log(parent.querySelector('.answer'))
-    parent.querySelector('.answer').classList.toggle('open')
 
-        document.querySelectorAll('.answer').forEach((answer) => {
-            if (answer !== parent.querySelector('.answer')) {
-                answer.classList.remove('open');
-            }
-        })
-    }) })
+questions.forEach(question => {
+  question.addEventListener('click', e => {
+
+    document.querySelectorAll('.answer').forEach(answer => {
+      answer.classList.remove('open')
+    })
+
+    questions.forEach(question => {
+      question.querySelector('i').classList.remove('fa-minus')
+      question.querySelector('i').classList.add('fa-plus')
+    })
+
+    question.querySelector('i').classList.add('fa-minus')
+    question.querySelector('i').classList.remove('fa-plus')
+
+    
+    const parent = question.parentElement
+    const answer = parent.querySelector('.answer')
+    answer.classList.add('open')
+
+  })
+})
