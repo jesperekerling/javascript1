90e917edc673284730e77e08b45a22358ad4f0e3
Jesper Ekerling <jesper@forme.se>
2023-11-07 13:28:17 +0100

    Update

---
+++ lektion-5/3-forms/script.js
@@ -0,0 +1,25 @@
+//console.log(document.forms);
+regForm = document.querySelector('#regForm');
+//console.log(regForm[0]);
+
+regForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const firstNameValue = regForm[0].value;
+    const lastNameValue = regForm.lastName;
+    const addressValue = document.querySelector('#address').value; // Teacher likes this way
+    const roleValue = e.currentTarget.role.value;
+    const termsValue = e.currentTarget.terms.value;
+
+    const user = {
+        firstName: firstNameValue,
+        lastNameValue: lastNameValue,
+        address: addressValue,
+        role: roleValue,
+    }
+
+    const terms = e.currentTarget.terms
+    if(!terms.checked) return;
+    console.log(terms.checked)
+    console.log(user);
+});
